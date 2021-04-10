0a5f218a77f2279d02f7230f101737b4ac7b1877
Dan Petrisko <petrisko@cs.washington.edu>
2021-04-10 13:59:30 -0700

    Initial commit

---
+++ soc/riscv/blackparrot/soc.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RISCV_BLACKPARROT_SOC_H_
+#define __RISCV_BLACKPARROT_SOC_H_
+
+#include <soc_common.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/arch/riscv/sys_io.h>
+
+#endif
